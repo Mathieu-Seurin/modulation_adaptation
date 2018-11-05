7c6874dbd3518ecb143f1c1efc54cdc1aec81b18
Seurin Mathieu <matseurin@cegetel.net>
2018-11-05 17:28:46 +0100

    Still struggling to split neural toolbox

--- src/neural_toolbox/gpu_utils.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-# GPU compatibility setup
-USE_CUDA = torch.cuda.is_available()
-FloatTensor = torch.cuda.FloatTensor if USE_CUDA else torch.FloatTensor
-LongTensor = torch.cuda.LongTensor if USE_CUDA else torch.LongTensor
-ByteTensor = torch.cuda.ByteTensor if USE_CUDA else torch.ByteTensor
-Tensor = FloatTensor
